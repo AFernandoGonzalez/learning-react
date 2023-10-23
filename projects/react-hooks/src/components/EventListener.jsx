ce98df832107d0c20f562043ebb5a9f5d6b80f19
Fernando <ferg.developer@gmail.com>
2023-10-23 19:36:07 -0400

    event listener app completed

--- projects/react-hooks/src/components/EventListener.jsx
+++ projects/react-hooks/src/components/EventListener.jsx
@@ -7,38 +7,67 @@ const colorsPalate = [
     "#CAC4B0",
     "#D84B20",
     "#AEA04B",
+    "#CAC3B0",
+    "#D44B20",
+    "#AEA02B",
     "#2A6478"
 ]
 
 export const EventListener = () => {
 
-    const [isActivated, setIsActivated] = useState(false)
+    const [isActivated, setIsActivated] = useState(false);
 
     useEffect(() => {
-        randomizeColor()
-console.log(randomizeColor());
-    }, [])
+        const divElement = document.getElementById('randomColors')
 
-    const randomizeColor = () => {
+        const changeColor = () => {
+            const newColorIndex = randomizedColorIndex()
+            divElement.style.backgroundColor = newColorIndex
+        }
+
+        if(isActivated){
+            divElement.addEventListener('mouseover', changeColor)
+        }else{
+            divElement.removeEventListener('mouseover', changeColor)
+        }
+
+        //clean up function
+        return (() => {
+            divElement.removeEventListener('mouseover', changeColor)
+        })
+
+    }, [isActivated])
+
+
+    const randomizedColorIndex = () => {
         const randomColorIndex = Math.floor(Math.random() * colorsPalate.length)
-
-      
         return colorsPalate[randomColorIndex]
     }
-
 
     return (
         <section style={{ backgroundColor: "rgba(75,75,75,.3)", padding: "1rem" }}>
             <h4>Event Listener</h4>
             <div>
                 <div
-                    style={{ height: "5rem", display: "flex", flexDirection: "column", alignContent: "center", justifyContent: "center" }} id="randomColors">
+                    style={{
+                        height: "5rem",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignContent: "center",
+                        justifyContent: "center",
+                        border: "1px solid white"
+                    }} id="randomColors">
                     Colors
                 </div>
                 <button
-                    style={{ color: "white" }}
-                    onClick={()=> setIsActivated(!isActivated)}
-                >{isActivated ? "Deactivate Listener" : "Activate Listener"}</button>
+                    style={{
+                        color: "white",
+                        margin: "1rem"
+                    }}
+                    onClick={() => setIsActivated(!isActivated)}
+                >
+                    {isActivated ? "Deactivate" : "Activate to Hover"}
+                </button>
             </div>
         </section>
     )
